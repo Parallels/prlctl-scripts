9de1c2c9bd8e88e3891cbd75f1c5d38716acdfc9
cjlapao <cjlapa@gmail.com>
2023-09-20 08:53:07 +0400

    fixed the scripts to dedupe machines

--- scripts/get-machines-all-users.sh
+++ scripts/get-machines-all-users.sh
@@ -26,26 +26,38 @@ done
 get_host_users() {
   dscl . list /Users | grep -v "^_" | grep '\S'
 }
+
 lines=""
+
+# Create an empty temporary file
+temp_file=$(mktemp /tmp/uuids.XXXXXX)
+temp_names=$(mktemp /tmp/names.XXXXXX)
 
 for user in $(get_host_users); do
   line=""
+  id=""
   [ -n "${user}" ] && [ -e "/Users/${user}" ] || continue
-  if [ "$FORMAT" = "csv" ]; then
-    line=$(sudo -u $user prlctl list -a | awk -v my_status="$STATUS" '$2 ~ my_status { $1=$2=$3=""; gsub(/^ */, ""); print }' | tr '\n' ',' | sed 's/,$//' | sed 's/NAME//')
-  else
-    line=$(sudo -u $user prlctl list -a | awk -v my_status="$STATUS" '$2 ~ my_status { $1=$2=$3=""; gsub(/^ */, ""); print }' | tr '\n' '|' | sed 's/|$//' | sed 's/NAME|//')
+
+  input_data=$(sudo -u $user prlctl list -a)
+  filtered_data=$input_data
+
+  if [ -n "$STATUS" ]; then
+    filtered_data=$(echo "$input_data" | awk -v filter="$STATUS" '$2 == filter {gsub(/[{}]/, "", $1); print $1}')
   fi
-  #echo $line
-  if [ -n "$line" ]; then
-    if [ -n "$lines" ]; then
-      lines="$lines | $line"
-    else
-      lines="$line"
+
+  echo "$filtered_data" | awk '{gsub(/[{}]/, "", $1); print $1}' | while read uuid; do
+    if ! grep -q "$uuid" "$temp_file"; then
+      if [ "$uuid" != "UUID" ]; then
+        echo "$uuid" >>"$temp_file"
+        machineName=$(echo "$input_data" | awk -v filter="$uuid" '$1 ~ filter { $1=$2=$3=""; gsub(/^ */, ""); print }')
+        echo "$machineName" >>"$temp_names"
+      fi
     fi
-  fi
+  done
 done
 
-lines=${lines//|/ | }
+lines=$(cat "$temp_names" | tr '\n' ',' | sed 's/,$//')
+rm "$temp_file"  # Cleanup the temporary file
+rm "$temp_names" # Cleanup the temporary file
 
 echo "<result>$lines</result>"
--- scripts/get-number-vms-all-users.sh
+++ scripts/get-number-vms-all-users.sh
@@ -1,13 +1,50 @@
-get_host_users() {
-	dscl . list /Users | grep -v "^_" | grep '\S'
-}
-user_count=0
+#/bin/bash
+STATUS=""
+FORMAT=""
 
+while [[ $# -gt 0 ]]; do
+  key="$1"
+
+  case $key in
+  -s | --status)
+    STATUS="$2"
+    shift # move past argument
+    shift # move past value
+    ;;
+  *)
+    # unknown option
+    shift # move past argument
+    ;;
+  esac
+done
+
+get_host_users() {
+  dscl . list /Users | grep -v "^_" | grep '\S'
+}
+
+# Create an empty temporary file
+temp_file=$(mktemp /tmp/uuids.XXXXXX)
 for user in $(get_host_users); do
   [ -n "${user}" ] && [ -e "/Users/${user}" ] || continue
-  count=$(sudo -u $user prlctl list -a | grep -oE '\{[a-f0-9\-]+\}' | wc -l | awk '{$1=$1};1')
-  user_count=$((user_count + count))
+
+  input_data=$(sudo -u $user prlctl list -a)
+  filtered_data=$input_data
+
+  if [ -n "$STATUS" ]; then
+    filtered_data=$(echo "$input_data" | awk -v filter="$STATUS" '$2 == filter {gsub(/[{}]/, "", $1); print $1}')
+  fi
+
+  echo "$filtered_data" | awk '{gsub(/[{}]/, "", $1); print $1}' | while read uuid; do
+    if ! grep -q "$uuid" "$temp_file"; then
+      if [ "$uuid" != "UUID" ]; then
+        echo "$uuid" >>"$temp_file"
+      fi
+    fi
+  done
 done
-echo "Total number of VMs for all users: $user_count"
 
+user_count=$(wc -l "$temp_file" | awk '{$1=$1};1' | cut -d' ' -f1)
 
+rm "$temp_file"  # Cleanup the temporary file
+
+echo "<result>$user_count</result>"
