ea6ea3504336c18f3cb98d1f781deed7270ac373
Carlos Lapao <cjlapao@gmail.com>
2023-11-28 09:09:54 +0000

    ghithub actions  runner script (#6)
    added a integral script that takes a pat token and automated the rest

---
+++ github/actions-runner/linux/remove-runner.sh
@@ -0,0 +1,61 @@
+#!/bin/bash
+
+while getopts ":o:t:p:u:" opt; do
+  case $opt in
+  p)
+    DESTINATION="$OPTARG"
+    ;;
+  o)
+    ORGANIZATION_NAME="$OPTARG"
+    ;;
+  t)
+    TOKEN="$OPTARG"
+    ;;
+  u)
+    RUN_AS="$OPTARG"
+    ;;
+  \?)
+    echo "Invalid option -$OPTARG" >&2
+    ;;
+  esac
+done
+
+if [ -z "$ORGANIZATION_NAME" ]; then
+  echo "Organization name is required"
+  exit 1
+fi
+
+if [ -z "$TOKEN" ]; then
+  echo "PAT Token is required"
+  exit 1
+fi
+
+if [ -z "$DESTINATION" ]; then
+  DESTINATION="$HOME/actions-runner"
+fi
+
+if [ -z "$RUN_AS" ]; then
+  RUN_AS=$USER
+fi
+
+function Remove() {
+    # Getting the registration token
+  echo "Removing the actions runner from $ORGANIZATION_NAME"
+  AUTH_TOKEN=$(curl -s -L -X POST -H "Accept: application/vnd.github+json" -H "Authorization: token $TOKEN" -H "X-GitHub-Api-Version: 2022-11-28" https://api.github.com/orgs/$ORGANIZATION_NAME/actions/runners/registration-token | jq -r '.token')
+  echo "Auth Token is $AUTH_TOKEN"
+
+  sudo -u $RUN_AS $DESTINATION/actions-runner/config.sh remove --token $AUTH_TOKEN
+}
+
+function Cleanup() {
+  echo "Cleaning up the installation"
+  sudo rm -rf $DESTINATION/actions-runner
+
+  echo "Removing the service"
+  rm -f /etc/systemd/system/actions.runner.service
+  systemctl daemon-reload
+}
+
+Remove
+Cleanup
+echo "Done"
