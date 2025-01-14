ef3eaba71891aff52adcd8005abbbc658049971e
Carlos Lapao <cjlapao@gmail.com>
2025-01-14 14:41:55 +0000

    updated version of the script

--- macOS/enable-remote-vnc.sh
+++ macOS/enable-remote-vnc.sh
@@ -31,37 +31,50 @@ sudo ./kickstart -configure -allowAccessFor -specifiedUsers
 sudo ./kickstart -configure -allowAccessFor -allUsers -privs -all
 sudo ./kickstart -activate
 
+# Disable sleep
+echo "Disabling sleep"
 sudo systemsetup -setcomputersleep Off
 systemsetup -setcomputersleep Off || true
-
 sudo pmset -a standby 0
+
 # Disable disk sleep
+echo "Disabling disk sleep"
 sudo pmset -a disksleep 0
+
 # Hibernate mode is a problem on some mac minis; best to just disable
+echo "Disabling hibernate mode"
 sudo pmset -a hibernatemode 0
 
 # Disable indexing volumes
+echo "Disabling indexing volumes"
 sudo defaults write ~/.Spotlight-V100/VolumeConfiguration.plist Exclusions -array "/Volumes"
 sudo defaults write ~/.Spotlight-V100/VolumeConfiguration.plist Exclusions -array "/Network"
 sudo killall mds
 sleep 60
+
 # Make sure indexing is DISABLED for the main volume
+echo "Disabling indexing"
 sudo mdutil -a -i off /
 sudo mdutil -a -i off
 
 # Disable Time Machine
+echo "Disabling Time Machine"
 sudo tmutil disable
 
 # Disable the screensaver
-defaults -currentHost write com.apple.screensaver idleTime 0
+echo "Disabling the screensaver"
+sudo defaults -currentHost write com.apple.screensaver idleTime 0
 
 # Disable the screensaver password
-defaults -currentHost write com.apple.screensaver askForPassword -int 0
+echo "Disabling the screensaver password"
+sudo defaults -currentHost write com.apple.screensaver askForPassword -int 0
 
 # Disable the screensaver password delay
-defaults -currentHost write com.apple.screensaver askForPasswordDelay -int 0
+echo "Disabling the screensaver password delay"
+sudo defaults -currentHost write com.apple.screensaver askForPasswordDelay -int 0
 
 if [[ -n "$USERNAME" ]]; then
+  echo "Creating user $USERNAME and setting password"
   sudo dscl . -passwd /Users/$USERNAME "$NEW_PASSWORD"
   sudo dscl . -create /Users/$USERNAME Picture "/Library/User Pictures/Nature/Earth.png"
   sudo /System/Library/CoreServices/RemoteManagement/ARDAgent.app/Contents/Resources/kickstart -configure -access -on -users $USERNAME -privs -all -restart -agent -menu
