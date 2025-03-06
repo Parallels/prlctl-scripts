b077aec95f32a6f01b867d22045139a65246750a
Aleksandr Sursiakov <sursyakov.a@gmail.com>
2025-03-06 16:38:54 -0500

    Update installParallelsDesktop.sh

--- intunes/installParallelsDesktop.sh
+++ intunes/installParallelsDesktop.sh
@@ -1,4 +1,9 @@
-The script is not longer required, the latest Parallels Autodeploy package works well with Microsoft InTune.
+READ
+The script is no longer required; the latest Parallels Autodeploy package works well with Microsoft InTune.
 
-Download the latest package and learn more how to tune it at: 
+Download the latest package and learn more about how to tune it at: 
 https://docs.parallels.com/parallels-desktop-business-administrators-guide/deploying-parallels-desktop-for-mac-business-edition/mass-deployment-using-mac-management-tools/preparing-the-autodeploy-package/downloading-the-autodeploy-package
+
+Also learn about a modern way of deployment using MDM Configurtion profiles, App catalogs and using Management portal to specify policies and golden images:
+https://docs.parallels.com/parallels-desktop-enterprise-administrators-guide/mass-deployment-of-parallels-desktop-and-virtual-machines/mass-deployment-using-mac-management-tools/recommended-deploying-parallels-desktop-via-mdm-app-catalogs-using-configuration-profiles
+https://docs.parallels.com/parallels-desktop-enterprise-administrators-guide/parallels-management-portal
